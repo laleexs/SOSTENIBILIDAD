deb75be9f467b5cc4ae5d11b0b2792c8cd4bc95b
laleexs <alex.sueca.3@gmail.com>
2025-10-31 19:08:11 +0100

    Quitando todos los elementos moviles del ejecicio 5 y poniendo la etiqueta lang en el cabezero de html para que de un mejor resultado de acesibilidad de 85 a 100 puntos en ligthouse

--- AP2/js/ejercicio5.js
+++ AP2/js/ejercicio5.js
@@ -16,14 +16,6 @@ function createGrid2(rows2, cols2, fill = false) {
 
 let grid2 = createGrid2(ROWS2, COLS2, false);
 
-function randomize2(p = 0.2) {
-  for (let r = 0; r < ROWS2; r++) {
-    for (let c = 0; c < COLS2; c++) {
-      grid2[r][c] = Math.random() < p ? 1 : 0;
-    }
-  }
-}
-randomize2(0.1); // 20% vivas
 
 function draw2() {
   ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
@@ -37,53 +29,6 @@ function draw2() {
   }
 }
 draw2();
-
-function neighbors2(r, c) {
-  let n = 0;
-  for (let dr = -1; dr <= 1; dr++) {
-    for (let dc = -1; dc <= 1; dc++) {
-      if (dr === 0 && dc === 0) continue;
-      const rr = (r + dr + ROWS2) % ROWS2;
-      const cc = (c + dc + COLS2) % COLS2;
-      n += grid2[rr][cc];
-    }
-  }
-  return n;
-}
-function step2() {
-  const next = createGrid2(ROWS2, COLS2, false);
-  for (let r = 0; r < ROWS2; r++) {
-    for (let c = 0; c < COLS2; c++) {
-      const alive = grid2[r][c] === 1;
-      const n = neighbors2(r, c);
-      next[r][c] =
-        (alive && (n === 2 || n === 3)) || (!alive && n === 3) ? 1 : 0;
-    }
-  }
-  grid2 = next;
-  draw2();
-}
-
-step2(); // prueba una generación
-
-let running2 = true;
-function loop2() {
-  if (running2) {
-    step2();
-  }
-  requestAnimationFrame(loop2);
-}
-loop2();
-// (Espacio) para pausar/reanudar
-/*
-document.addEventListener("keydown", (e) => {
-  if (e.key === " ") {
-    running2 = !running2;
-    e.preventDefault();
-  }
-});
-*/
-
 
 // Insertar un blinker 
 function setBlinker(r, c) {
@@ -107,7 +52,7 @@ function setToad(r, c) {
 }
 
 setToad(40,40);
-draw2();
+
 
  // Insertar un glider
  function setGlider(r, c) {
@@ -118,6 +63,9 @@ draw2();
  grid2[r-2][c+1] = 1;
  }
  setGlider(60,50);
+
+ draw2();
+
 
  
 
